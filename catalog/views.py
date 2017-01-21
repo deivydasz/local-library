83c4304e00de761312a1ff8ac3a2380b4de103b3
ecuSinner <deivydas.ziziliauskas@gmail.com>
2017-01-22 01:40:00 +0200

    Added visits counter using session

--- catalog/views.py
+++ catalog/views.py
@@ -12,18 +12,21 @@ def index(request):
 	# Available books (status = 'a')
 	num_instances_available = BookInstance.objects.filter(status__exact='a').count()	
 	num_authors = Author.objects.count()
-
 	num_genres = Genre.objects.count()
 	num_languages = Language.objects.count()
-
 	num_books_ender = Book.objects.filter(title__contains="Ender").count()
+
+	# Number of visits to this view, as counted in the session variable.
+	num_visits=request.session.get('num_visits', 0)
+	request.session['num_visits'] = num_visits+1
 
 	return render(
 		request,
 		'index.html',
 		context={'num_books':num_books, 'num_instances':num_instances,
 			'num_instances_available':num_instances_available, 'num_authors':num_authors,
-			'num_genres':num_genres, 'num_languages':num_languages, 'num_books_ender':num_books_ender}
+			'num_genres':num_genres, 'num_languages':num_languages, 'num_books_ender':num_books_ender,
+			'num_visits':num_visits}
 	)
 
 from django.views import generic
