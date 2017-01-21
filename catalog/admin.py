1c6a5f50755aab5e8a12b7c380b338552f407395
ecuSinner <deivydas.ziziliauskas@gmail.com>
2017-01-21 22:31:22 +0200

    Registered models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Author, Genre, Language, Book, BookInstance
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(BookInstance)
+
