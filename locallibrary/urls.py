597014072549f6c1e8f835d47de99ae95e0bce7b
ecuSinner <deivydas.ziziliauskas@gmail.com>
2017-01-21 20:22:45 +0200

    Added basic routes

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -19,3 +19,22 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+# Use include() to add URLS from the catalog application 
+from django.conf.urls import include
+
+urlpatterns += [
+    url(r'^catalog/', include('catalog.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
